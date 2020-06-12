daf3a5d16ddc97620f576786595bdf6cdd5eadd9
Zvonimir Hrastić <zhrastic@gmail.com>
2020-06-12 14:06:38 +0200

    Local

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -27,7 +27,7 @@ namespace WebApp
                     ContractResolver = new CamelCasePropertyNamesContractResolver()
                 };
 
-            // Attribute routing.
+            // Attribute routing. Pero
             httpConfiguration.MapHttpAttributeRoutes();
             httpConfiguration.Routes.MapHttpRoute(
                 name: "DefaultApi",
